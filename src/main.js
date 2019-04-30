1249b4327da8c650cb4ba2f1cef78de57acbd3c2
myishc <844271143@qq.com>
2019-04-30 10:13:56 +0800

    初始化提交2019-4-30 10:15

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
+
+
+//导入需要的模块/组件
 import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
+//设置路由地址
+const routes=[
+
+]
+
+//实例路由对象
+const router = {
+  routes
+}
+
+//挂载路由
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
