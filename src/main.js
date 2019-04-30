6ed10aa84eff45f5b6f6d313f52c0f3d4ffbf1ac
myishc <844271143@qq.com>
2019-04-30 11:29:36 +0800

    首页商品展示

--- src/main.js
+++ src/main.js
@@ -1,21 +1,27 @@
 
 
-//导入需要的模块/组件
+//导入需要的模块
 import Vue from 'vue'
-import App from './App.vue'
+import VueRouter from 'vue-router'
+//导入头部底部公共样式
 import './assets/statics/site/css/style.css'
+//导入组件
+import App from './App.vue'
+import index from './components/index.vue'
+
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
 
 //设置路由地址
 const routes=[
-
+  {path: '/index', component: index}
 ]
 
 //实例路由对象
-const router = {
+const router = new VueRouter({
   routes
-}
+}) 
 
 //挂载路由
 new Vue({
